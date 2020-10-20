0e6d39d8537ec6b9138a30a2ce530374829d8f19
EmilEinarsen <47479113+EmilEinarsen@users.noreply.github.com>
2020-10-20 09:50:10 +0200

    Added support for sessions, local and cookie storage

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 import { useState } from 'react'
+import { set as setCookie, get as getCookie } from 'bjork_cookie'
 
 export const TYPES = {
 	LOCAL: 'local',
-	SESSION: 'session'
+	SESSION: 'session',
+	COOKIE: 'cookie'
 }
 
-function useStorage(type, key, initialValue) {
-	const storage = 
-		type === TYPES.LOCAL ? localStorage 
-		: type === TYPES.SESSION ? sessionStorage 
-		: ''
+const getStorageGetSet = type => (
+	type === TYPES.LOCAL ? {
+		getItem: key => localStorage.getItem(key),
+		setItem: (key, value) => localStorage.setItem(key, value),
+	} : type === TYPES.SESSION ? {
+		getItem: key => sessionStorage.getItem(key),
+		setItem: (key, value) => sessionStorage.setItem(key, value),
+	} :  type === TYPES.COOKIE ? {
+		getItem: key => getCookie(key),
+		setItem: (key, value, options) => setCookie(key, value, options),
+	} : {}
+)
 
-	const [item, setItem] = useState(storage.getItems(key, initialValue))
+function useStorage(type, key, initialValue = '') {
+
+	const storage = getStorageGetSet(type)
+
+	const [item, setItem] = useState(() => (
+		storage.getItem(key) ? storage.getItem(key) 
+		: () => {
+			storage.setItem(key, initialValue)
+			return initialValue
+		}
+	))
   
 	return [
 		item, 
-		(value, options) => {
+		type === TYPES.COOKIE ? (value, options) => {
 			setItem(value)
 			storage.setItem(key, value, options)
-	 	 }
+		} : value => {
+			setItem(value)
+			storage.setItem(key, value)
+		}
 	]
 }
 
